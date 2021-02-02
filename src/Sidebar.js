c26bbd8674ab9da8de684441d6b8c4239c0be2ff
Robzlegz <robzlegz@gmail.com>
2021-02-02 10:35:17 +0200

    creating new pages and updating database

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Sidebar.css";
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChat from "./SidebarChat";
 import db from "./firebase";
+import { Room } from '@material-ui/icons';
 
 const Sidebar = () => {
+
+    const [channels, setChannels] = useState([]);
+
+    useEffect(() => {
+        db.collection("channels").onSnapshot((snapshot) => (
+            setChannels(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            })))
+        ));
+    }, []);
 
     const createChannel = () => {
         const channelName = prompt("Enter a name for a channel");
         if(channelName){
-            
+            db.collection("channels").add({
+                channelName: channelName,
+            });
         }
     }
 
@@ -20,7 +34,9 @@ const Sidebar = () => {
                 <AddIcon onClick={createChannel} classname="addChatButton" />
             </div>
             <div className="sidebar-chats">
-                <SidebarChat />
+                {channels.map((channel) => (
+                    <SidebarChat key={channel.id} id={channel.id} name={channel.data.channelName} />
+                ))}
             </div>
         </div>
     );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,18 +2,18 @@ import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import "./SidebarChat.css";
 
-const SidebarChat = () => {
+const SidebarChat = ({id, name}) => {
 
     const [avatarSeed, setAvatarSeed] = useState("");
 
     useEffect(() => {
         setAvatarSeed(Math.floor(Math.random() * 5000));
-    }, [])
+    }, []);
 
     return (
         <div className="sidebarChat">
             <Avatar className="sidebarChatAvatar" src={`https://avatars.dicebear.com/api/identicon/${avatarSeed}.svg`} />
-            <p>ChatName</p>
+            <p>{name}</p>
         </div>
     );
 }
