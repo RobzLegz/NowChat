93cba012aaf6c6313714ff007298075349a34291
Robzlegz <robzlegz@gmail.com>
2021-02-02 11:14:55 +0200

    user autentification finished

---
+++ src/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { auth, provider } from './firebase';
+import "./Login.css";
+import useStateValue from './StateProvider';
+
+const Login = () => {
+    
+    const [{user}, dispatch] = useStateValue();
+
+    const login = () => {
+        auth.signInWithPopup(provider).then((result) => {
+            dispatch({
+                type: "SET_USER",
+                user: result.user,
+            })
+        }).catch((error) => alert(error.message));
+    }
+
+    return (
+        <div className="login">
+            <img src="https://cdn.dribbble.com/users/31864/screenshots/3666062/free_logos_dribbble_ph.jpg?compress=1&resize=800x600" alt="logo"/>
+            <button onClick={login}>Sign In</button>
+        </div>
+    );
+}
+
+export default Login;
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,19 @@
 export const initialState = {
-    user: "rob",
+    user: null,
 };
 
 export const reducer = (state, action) => {
     switch(action.type){
-        
+        case "SET_USER":
+            return{
+                ...state,
+                user: action.user,
+            };
+        default:
+            return{
+                ...state,
+            };
     }
-}
+};
+
+export default reducer;
