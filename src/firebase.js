117fe919964d7c27c59c339eb4e14a46b69350c0
Robzlegz <robzlegz@gmail.com>
2021-02-02 08:18:07 +0200

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ4LNUgUHGkbLNVQSzGQcPmRPyrANTybI",
+    authDomain: "discord-clone-5bca3.firebaseapp.com",
+    projectId: "discord-clone-5bca3",
+    storageBucket: "discord-clone-5bca3.appspot.com",
+    messagingSenderId: "575088879857",
+    appId: "1:575088879857:web:61e4c64278a4e2c4da2150",
+    measurementId: "G-QY9LZ8CS0M"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
