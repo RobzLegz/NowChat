700e4730d4cac193a80f99ca7eef14583e1988d7
Robzlegz <robzlegz@gmail.com>
2021-02-02 09:24:26 +0200

    styling and avatar update

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import { Avatar } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import "./SidebarChat.css";
 
 const SidebarChat = () => {
+
+    const [avatarSeed, setAvatarSeed] = useState("");
+
+    useEffect(() => {
+        setAvatarSeed(Math.floor(Math.random() * 5000));
+    }, [])
+
     return (
-        <div>
-            sidebarchat
+        <div className="sidebarChat">
+            <Avatar className="sidebarChatAvatar" src={`https://avatars.dicebear.com/api/identicon/${avatarSeed}.svg`} />
+            <p>ChatName</p>
         </div>
     );
 }
